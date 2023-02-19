da1a426dce25e822c9a54933b1cae9d3515c6777
amanikaneza <akanezamulume.iuea@gmail.com>
2023-02-19 16:51:10 +0300

    learning js

--- chapter-2/e.js
+++ chapter-2/e.js
@@ -3,9 +3,6 @@
  * to console.log to output a triangle made with
  * hash-tag
  */
-for (let a = 0; a <= 7; a += 1) {
-  console.log("amani\n kaneza");
-  for (let b = 0; b <= 7; b += 1) {
-    console.log("#");
-  }
+for (let line = "#"; line.length < 8; line += "#") {
+  console.log(line);
 }
--- chapter-2/g.js
+++ chapter-2/g.js
@@ -4,9 +4,12 @@
  * Write a program a string that represent a 8x8 grid,
  * using new-line character to separate lines.
  */
-for (let line = 0; line <= 8; line += 1) {
-  console.log("#");
-  for (let character = 0; character <= 8; character += 1) {
-    console.log("#");
+
+for (let line = 0; line < 9; line += 1) {
+  for (let lineboard = "#"; ; lineboard += "#") {
+    if (lineboard.length == 8) {
+      console.log(lineboard);
+      break;
+    }
   }
 }
