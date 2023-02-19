76ca124fc2eddc53b7f95cd08fce04dff1586d96
amanikaneza <akanezamulume.iuea@gmail.com>
2023-02-19 16:01:48 +0300

    learning js

--- chapter-2/f.js
+++ chapter-2/f.js
@@ -1,9 +1,24 @@
 /*
  * FizzBuzz
- * Write a program thar uses console.log to print all numbers
+ * Write a program that uses console.log to print all numbers
  * from 1 t0 100, with two exceptions.
  * For number divisible by 3,print 'Fizz'
  * instead of number.
  * and for the numbers divisible by five
  *  print 'Buzz' instead.
+ *
+ * Modify the program to print 'FizzBuzz' for numbers that are both
+ * divisible by 3 and 5 (and still printing 'Fizz' or 'Buzz' for number
+ * for number that are only divisible by one of those)
  */
+let number = 1;
+while (number <= 100) {
+  if (number % 3 === 0) {
+    console.log("Fizz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log("FizzBuzz");
+  }
+  number += 1;
+}
---
+++ chapter-2/g.js
@@ -0,0 +1,12 @@
+/*
+ * ChessBoard
+ *
+ * Write a program a string that represent a 8x8 grid,
+ * using new-line character to separate lines.
+ */
+for (let line = 0; line <= 8; line += 1) {
+  console.log("#");
+  for (let character = 0; character <= 8; character += 1) {
+    console.log("#");
+  }
+}
